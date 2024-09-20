bf3f5192710a023a9838beef7f6fc96eafc82a26
Bartek Pierściński <bartoszjpierscinski@gmail.com>
2024-09-20 19:08:27 +0200

    ta

--- components/Cookie.vue
+++ components/Cookie.vue
@@ -9,7 +9,10 @@
 				The only interaction I have is with my computer. I don't know. I can't even call
 				it interaction. Further details can be found in the privacy information.
 			</div>
-			<div @click="update({ cookie: false })" class="close">Accept</div>
+			<div class="buttons">
+				<div @click="update({ cookie: false })" class="close">Accept</div>
+				<div @click="update({ cookie: false })" class="close">Reject</div>
+			</div>
 		</div>
 	</transition>
 </template>
@@ -24,21 +27,28 @@
 <style lang="postcss" scoped>
 	.cookie {
 		background-color: var(--darkgrey);
+		color: white !important;
 		bottom: var(--space-m);
 		left: var(--space-m);
+		top: initial !important;
 		width: calc(100vw - 2 * var(--space-m));
 		padding: var(--space-m);
 		display: flex;
 		flex-direction: row;
 		gap: var(--space-l);
-		border-radius: 3px;
-		& > .close {
-			background-color: white;
-			padding: var(--space-s) var(--space-l);
-			height: fit-content;
-			color: var(--darkgrey);
-			cursor: pointer;
-			border-radius: 3px;
+		border-radius: var(--border-radius);
+		& > .buttons {
+			display: flex;
+			flex-direction: row;
+			gap: var(--space-s);
+			& > .close {
+				background-color: white;
+				padding: var(--space-s) var(--space-l);
+				height: fit-content;
+				color: var(--darkgrey);
+				cursor: pointer;
+				border-radius: var(--border-radius);
+			}
 		}
 	}
 </style>
